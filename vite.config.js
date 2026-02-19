6df2d74414768fe5c97af4d7e02f7b27749c868c
DivPanda <115326631+DivPanda@users.noreply.github.com>
2026-02-19 18:11:01 -0400

    Fix base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Proyecto-catalogo-de-peliculas-y-series/'
+  base: '/Proyecto-catalogo-de-peliculas-y-series/',
 })
